41582e1bd10029493a0e1c2c8b84cd98b12160b2
faizanvahevaria <faizanvahevaria@gmail.com>
2019-07-04 15:35:47 +0530

    # Attribute class - wip : Add Info class

---
+++ info.js
@@ -0,0 +1,31 @@
+/**
+ *
+ *
+ * @class Info
+ */
+class Info {
+     /**
+      * Creates an instance of Info.
+      * 
+      * @constructor Info
+      */
+     constructor(mainElem) {
+          this.mainInfoObj={};
+          this.mainInfoObj.description=mainElem.name+ ' API - 1.0.0';
+          this.mainInfoObj.title=mainElem.name+' API';
+          this.mainInfoObj.version='1.0.0';
+     }
+
+     
+     /**
+      * Return Info object 
+      * 
+      * @function getData
+      * @return {string}
+      */
+     getInfo() {
+          return this.mainInfoObj;
+     }
+}
+
+exports.Info = Info;
