ef2f5738dac4226b10d5b030e78f0772c87559b1
Faizan Vahevaria <faizanvahevaria@gmail.com>
2019-07-16 07:38:08 +0530

    src/constant.js: beautify code

--- src/constant.js
+++ src/constant.js
@@ -1,18 +1,18 @@
-const Constant={
-     entry:'#',
-     components:'components',
-     schemas:'schemas',
-     path:'/',
-     shared:'shared',
-     msgsuccess:'OpenAPI generation completed',
-     msg_file_select:'Select one of the following type.',
-     msg_file_saveas:'Save File as...',
+const Constant = {
+     entry: '#',
+     components: 'components',
+     schemas: 'schemas',
+     path: '/',
+     shared: 'shared',
+     msgsuccess: 'OpenAPI generation completed',
+     msg_file_select: 'Select one of the following type.',
+     msg_file_saveas: 'Save File as...',
      PREF_DEBUG_KEY : 'openapi:debug.status',
      PREF_GENDOC : 'openapi.gen.idlDoc',
      PREF_INDENTSPC : 'openapi.gen.indentSpaces'
 }
-function getRef(){
-     return Constant.entry+Constant.path+Constant.components+Constant.path+Constant.schemas+Constant.path;
+function getRef() {
+     return Constant.entry + Constant.path + Constant.components + Constant.path + Constant.schemas + Constant.path;
 }
-module.exports=Constant
-module.exports.getReference=getRef;
+module.exports = Constant
+module.exports.getReference = getRef;
