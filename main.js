5ba66853aaa924a9722939d6be383a714890572e
faizanvahevaria <faizanvahevaria@gmail.com>
2019-07-30 19:08:41 +0530

    set & get  AppMode &  TestMode execute command to test entire project

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var path = require('path');
 function _handleGenerate(umlPackage, options = getGenOptions()) {
      // If options is not passed, get from preference
 
-     openAPI.setMode(0); //0 mode for Generate API
+     openAPI.setAppMode(openAPI.APP_MODE_GEN); //0 mode for Generate API
      // If umlPackage is not assigned, popup ElementPicker
      if (!umlPackage) {
           app.elementPickerDialog
@@ -101,253 +101,140 @@ function getPackageOptions(ownedElements) {
                umlPackageList.push(mObject);
 
 
-               
+
 
 
           }
      });
      return umlPackageList;
 }
-/**
- * @function getAllPackageOptions
- * @param {Array} ownedElements
- * @description Returns the array of All Package list 
- * @returns {Array}
- */
-function getAllPackageOptions(ownedElements) {
-     let umlPackageList = [];
-     ownedElements.filter((item, index) => {
-
-          if (item instanceof type.UMLPackage) {
 
 
-               // item.ownedElements.filter((item, index) => {
-
-                    // if (item instanceof type.UMLPackage) {
-
-                         umlPackageList.push(item);
-
-                    // }
-
-               // });
-          }
-     });
-     return umlPackageList;
-}
-function hasSubPackages(item){
-     testPkgList=[];
-     item.ownedElements.filter((item, index) => {
-
-          if (item instanceof type.UMLPackage) {
-               testPkgList.push(item);
-          }
-     });
-     if(testPkgList.length==0){
-          return false;
-     }
-     else{
-          return true;
-     }
-}
 /**
  * @function _handleTestExtension
  * @description Handle test case for valid OpenApi Specification
  */
 function _handleTestExtension() {
 
-     openAPI.setMode(1); //1 mode for Test API
-     console.log("Main Object", app.project.project.ownedElements);
-     let umlPackageList = getPackageOptions(app.project.project.ownedElements);
-     var mObject = {};
-     mObject.text = "ALL";
-     mObject.value = umlPackageList.length;
-     umlPackageList.push(mObject);
 
-     let testPkgList = app.project.project.ownedElements;
-     app.dialogs.showSelectDropdownDialog(constant.msg_package_select, umlPackageList).then(function ({
-          buttonId,
-          returnValue
-     }) {
-          if (buttonId === 'ok') {
+     openAPI.setAppMode(openAPI.APP_MODE_TEST);
+     openAPI.setTestMode(openAPI.TEST_MODE_SINGLE);
+     app.elementPickerDialog
+               .showDialog("Select the package or project to test", null, null) //type.UMLPackage
+               .then(function ({
+                    buttonId,
+                    returnValue
+               }) {
+                    if (buttonId === "ok") {
+                         if (returnValue instanceof type.Project || returnValue instanceof type.UMLPackage) { //|| returnValue instanceof type.UMLPackage
+                              umlPackage = returnValue;
+                              testSinglePackage(umlPackage);
 
-
-
-
-
-               let selPackage = umlPackageList[returnValue];
-               if (selPackage.text == 'ALL') {
-                    testPkgList = getAllPackageOptions(app.project.project.ownedElements);
-                    console.log("Package selected", testPkgList);
-                    let tmpPkgList=[];
-                    testPkgList.filter((item, index) => {
-
-                         if (item instanceof type.UMLPackage) {
-                              // tmpPkgList=[];
-                              // testPkgList.push(item);  
-                              if(hasSubPackages(item)){
-                                   
-                                   item.ownedElements.filter((item, index) => {
-
-                                        if (item instanceof type.UMLPackage) {
-                                             tmpPkgList.push(item);
-                                        }
-                                   });
-                              }
-                              else{
-                                   tmpPkgList.push(item);  
-                              }
+                         } else {
+                              app.dialogs.showErrorDialog("Please select the project or a package");
                          }
-                    });
-
-
-                    testApi(2, tmpPkgList);
-               } else {
-                    testPkgList.filter((item, index) => {
-
-                         if (item.name == selPackage.text && (item instanceof type.UMLPackage)) {
-                              testPkgList=[];
-                              // testPkgList.push(item);  
-                              if(hasSubPackages(item)){
-                                   
-                                   item.ownedElements.filter((item, index) => {
-
-                                        if (item instanceof type.UMLPackage) {
-                                             testPkgList.push(item);
-                                        }
-                                   });
-                              }
-                              else{
-                                   testPkgList.push(item);  
-                              }
-                         }
-                    });
-
-                    umlPackageList = getPackageOptions(testPkgList);
-                    if (umlPackageList.length == 0) {
-                         app.toast.info(constant.msgpackage);
-                         return;
-                    } else if (umlPackageList.length == 1 && !hasSubPackages(testPkgList[0])) {
-                         // if(!hasSubPackages(testPkgList[0])){
-                              testApi(1, testPkgList[0]);
-                         // }
-                         // let tempTest = [];
-                         // testPkgList[0].ownedElements.filter((item, index) => {
-
-                         //      if (item instanceof type.UMLPackage) {
-                         //           tempTest.push(item);
-                         //      }
-                         // });
-                         // if (tempTest.length == 0) {
-                         //      testApi(1, testPkgList[0]);
-                         // }
-                    } else {
-                         app.dialogs.showSelectDropdownDialog(constant.msg_package_select, umlPackageList).then(function ({
-                              buttonId,
-                              returnValue
-                         }) {
-                              if (buttonId === 'ok') {
-
-                                   selPackage = umlPackageList[returnValue];
-
-                                   testPkgList.filter((item, index) => {
-
-                                        if (item.name == selPackage.text && (item instanceof type.UMLPackage)) {
-
-                                             console.log("Package selected", item);
-                                             console.log("testPkgList", testPkgList);
-
-                                             testApi(1, item);
-
-                                        }
-                                   });
-                              }
-                         });
                     }
-               }
+               });
+}
+/**
+ * @function removeOutputFiles
+ * @description Removes previously test generated .json files from the output folder
+ */
+function removeOutputFiles() {
+     const directory = __dirname + constant.IDEAL_TEST_FILE_PATH;
 
+     if (!fs.existsSync(directory)) {
+          fs.mkdirSync(directory);
+     }
 
+     fs.readdir(directory, (err, files) => {
+          if (err) throw err;
 
-
-
-
-          } else {
-               console.log("Package Cancelled")
+          for (const file of files) {
+               fs.unlink(path.join(directory, file), err => {
+                    if (err) throw err;
+               });
           }
      });
-     /**
-      * @function removeOutputFiles
-      * @description Removes previously test generated .json files from the output folder
-      */
-     function removeOutputFiles() {
-          const directory = __dirname + constant.IDEAL_TEST_FILE_PATH;
+}
+/**
+ * @function testSinglePackage
+ * @params {UMLPackage} item
+ * @description Test Single Package 
+ */
+function testSinglePackage(item) {
+     removeOutputFiles();
+     generateTestAPI(item)
+}
+/**
+ * @function testAllPackage
+ * @params {UMLPackage} item
+ * @description Test All Packages of Project
+ */
+function testAllPackage(item) {
 
-          if (!fs.existsSync(directory)) {
-               fs.mkdirSync(directory);
-          }
 
-          fs.readdir(directory, (err, files) => {
-               if (err) throw err;
+     removeOutputFiles();
 
-               for (const file of files) {
-                    fs.unlink(path.join(directory, file), err => {
-                         if (err) throw err;
+     let strSummery='';
+     asyncLoop(item, function (umlPackage, next) {
+          setTimeout(function () {
+               generateTestAPI(umlPackage)
+               next();
+          }, 1000);
+
+     }, function (err) {
+          if (err) {
+               console.error('Error: ' + err.message);
+               return;
+          }else{
+
+               setTimeout(function () {
+                    let summery=openAPI.getSummery();
+                    
+                    summery.filter((item, index) => {
+          
+                         strSummery+=item.message+'\n\n';
                     });
-               }
-          });
-     }
-
-     /**
-      * @function testApi
-      * @params {Integer} type
-      * @params {UMLPackage} item
-      * @description Test selected package 
-      */
-     function testApi(type, item) {
-
-
-          removeOutputFiles();
-
-          if (type == 1) {
-               generateTestAPI(item)
-          } else if (type == 2) {
-
-               asyncLoop(item, function (umlPackage, next) {
-                    setTimeout(function () {
-                         generateTestAPI(umlPackage)
-                         next();
-                    }, 1000);
-
-               }, function (err) {
-                    if (err) {
-                         console.error('Error: ' + err.message);
-                         return;
-                    }
-
-                    console.log('Finished!');
-               });
-
-
+                    app.dialogs.showAlertDialog(strSummery);
+               }, 1500);
           }
-     }
+     });
+}
 
-     /**
-      * @function generateTestAPI
-      * @params {UMLPackage} umlPackage
-      * @description Generate test api 
-      * */
-     async function generateTestAPI(umlPackage) {
+/**
+ * @function generateTestAPI
+ * @params {UMLPackage} umlPackage
+ * @description Generate test api 
+ * */
+async function generateTestAPI(umlPackage) {
 
-          const basePath = __dirname + constant.IDEAL_TEST_FILE_PATH;
-          // const umlPackage = testPkgList[0];
-          const options = getGenOptions();
+     const basePath = __dirname + constant.IDEAL_TEST_FILE_PATH;
+     // const umlPackage = testPkgList[0];
+     const options = getGenOptions();
 
-          const mOpenApi = new openAPI.OpenApi(umlPackage, basePath, options, 1);
-          await mOpenApi.initUMLPackage();
+     const mOpenApi = new openAPI.OpenApi(umlPackage, basePath, options, 1);
+     await mOpenApi.initUMLPackage();
 
-     }
+}
+/**
+ * @function _handleTestExtension
+ * @description Handle test case for valid OpenApi Specification
+ */
+function _handleAllTestPackages() {
+     var packages = app.repository.select("@UMLPackage")
 
-
-
+     openAPI.resetSummery();
+     openAPI.setAppMode(openAPI.APP_MODE_TEST);
+     openAPI.setTestMode(openAPI.TEST_MODE_ALL);
+     
+     let mPackages=[];
+     packages.forEach(element => {
+          if (element instanceof type.UMLPackage) {
+               mPackages.push(element);
+          }
+     });
+     testAllPackage(mPackages);
 }
 /**
  * @function init
@@ -356,6 +243,7 @@ function _handleTestExtension() {
 function init() {
      app.commands.register('generate:show-toast', _handleGenerate);
      app.commands.register('testextension:test-extension', _handleTestExtension)
+     app.commands.register('testallextension:test-all-extension', _handleAllTestPackages)
 }
 
 exports.init = init
