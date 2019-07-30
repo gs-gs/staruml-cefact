035a9cc99347d3d23d6b1294b71ed78d9e970d5e
faizanvahevaria <faizanvahevaria@gmail.com>
2019-07-30 16:10:14 +0530

    Add path constants for MDJ and .json file

--- src/constant.js
+++ src/constant.js
@@ -5,11 +5,17 @@ const Constant = {
      path: '/',
      shared: 'shared',
      msgsuccess: 'OpenAPI generation completed',
+     msgpackage: 'Package not available',
      msg_file_select: 'Select one of the following type.',
+     msg_package_select: 'Select one of the following package.',
      msg_file_saveas: 'Save File as...',
      PREF_DEBUG_KEY: 'openapi:debug.status',
      PREF_GENDOC: 'openapi.gen.idlDoc',
-     PREF_INDENTSPC: 'openapi.gen.indentSpaces'
+     PREF_INDENTSPC: 'openapi.gen.indentSpaces',
+     IDEAL_MDJ_FILE_PATH: '/src/model',
+     IDEAL_MDJ_FILE_NAME: 'SampleModel.mdj',
+     IDEAL_TEST_FILE_PATH: '/src/output',
+     IDEAL_JSON_FILE_NAME: 'SampleModel.json'
 }
 
 function getRef() {
