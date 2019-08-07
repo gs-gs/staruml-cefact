98e68936add1b25903800bc02a6a574bc0402691
faizanvahevaria <faizanvahevaria@gmail.com>
2019-08-07 10:39:52 +0530

    Add documentation of UMLPackage in info object(title, description)

--- src/info.js
+++ src/info.js
@@ -1,5 +1,6 @@
 const openAPI = require('./openapi');
 const version=require('../package.json').version
+const constant = require('../src/constant');
 /**
  *
  *
@@ -16,9 +17,27 @@ class Info {
       * @constructor Info
       */
      constructor() {
+          var version='';
+          var description='';
+          if(openAPI.getUMLPackage().hasOwnProperty('documentation')){
+               var re = openAPI.getUMLPackage().documentation.split("\n");
+               if(re.length>=1){
+                    var verArr=re[0].split(":")
+                    if(verArr.length>1){
+                         version=verArr[1];
+                    }
+               }
+               if(re.length>1){
+                    description=re[1]+'<br><br>'+constant.msg_description;
+               }
+               console.log("version",version);
+               console.log("description",description);
+               
+               
+          }
           this.mainInfoObj = {};
-          this.mainInfoObj.description = openAPI.getUMLPackage().name + ' API - '+version;
-          this.mainInfoObj.title = openAPI.getUMLPackage().name + ' API';
+          this.mainInfoObj.description = description;
+          this.mainInfoObj.title = openAPI.getUMLPackage().name;
           this.mainInfoObj.version = version;
      }
 
