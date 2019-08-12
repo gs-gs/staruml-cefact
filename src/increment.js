38da99f399a32492a218c803a3d67695a74fcd62
faizanvahevaria <faizanvahevaria@gmail.com>
2019-08-12 16:25:04 +0530

    Remove commented code and function file

--- src/increment.js
+++
@@ -1,7 +0,0 @@
-function increment(value) {
-    if (typeof value !== "number") {
-        throw "not a number";
-    }
-    return value + 1;
-}
-module.exports = increment;
--- test/test.js
+++ test/test.js
@@ -1,50 +1,24 @@
-var increment = require('../src/increment');
 var assert = require('assert');
 
 
 describe('OpenAPI AutoMation Testing', function () {
 
-
-  // console.log(JSON.stringify(mdjFile));
   it('Test Openapi', function () {
-
-    // validator.validate(json, openApiDocument, function (err, valid) {
-    //   if(err){
-    //     console.log("error",err);
-    //   }else{
-    //     console.log("success",valid);
-    //   }
-
-    // });
-
-    var result = increment(1);
-    // assert.equal(result, 2);
     assert.equal(0, 0);
   });
 
   it('Test Info', function () {
-    var result = increment(-10);
-    // assert.equal(result, -9);
     assert.equal(0, 0);
   });
 
   it('Test Server', function () {
-    // assert.throws(function() {
-    //   increment("purple");
-    // });
     assert.equal(0, 0);
   });
 
   it('Test Path', function () {
-    // assert.throws(function() {
-    //   increment("purple");
-    // });
     assert.equal(0, 0);
   });
   it('Test Component', function () {
-    // assert.throws(function() {
-    //   increment("purple");
-    // });
     assert.equal(0, 0);
   });
 
