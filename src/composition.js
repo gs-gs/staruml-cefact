4c7f035b1f213e3dea9d94bac08ebd42cbf19a06
Nis Jespersen <34443212+nissimsan@users.noreply.github.com>
2019-08-23 13:46:12 +0200

    Update composition.js
    I'm pretty sure the direction of relationships are reversed. It works for the SampleModel.mdj, but not when you make any new relationships. I have a feeling the SampleModel has been generated in some funny way, and that's what you've just aligned to.

--- src/composition.js
+++ src/composition.js
@@ -23,19 +23,19 @@ class Composition {
      addComposition(mainPropertiesObj, assoc) {
           let propertiesObj = {};
           mainPropertiesObj[assoc.name] = propertiesObj;
-          if (assoc.end2.multiplicity === "0..*" || assoc.end2.multiplicity === "1..*") {
+          if (assoc.end1.multiplicity === "0..*" || assoc.end1.multiplicity === "1..*") {
                let itemsObj = {};
                propertiesObj.items = itemsObj;
-               itemsObj['$ref'] = constant.getReference() + assoc.end2.reference.name;
+               itemsObj['$ref'] = constant.getReference() + assoc.end1.reference.name;
                propertiesObj.type = 'array';
                /**
                 * Add MinItems of multiplicity is 1..*
                 */
-               if (assoc.end2.multiplicity === "1..*") {
+               if (assoc.end1.multiplicity === "1..*") {
                     propertiesObj.minItems = 1;
                }
           } else {
-               propertiesObj['$ref'] = constant.getReference() + assoc.end2.reference.name;
+               propertiesObj['$ref'] = constant.getReference() + assoc.end1.reference.name;
           }
           return mainPropertiesObj;
      }
