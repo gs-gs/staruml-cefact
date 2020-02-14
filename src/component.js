57685be7327c830e7f996f3e98ac3ae45fd6e00d
faizanvahevaria <faizanvahevaria@gmail.com>
2020-02-14 10:48:50 +0530

    display alert instead throwing error

--- src/component.js
+++ src/component.js
@@ -60,7 +60,8 @@ class Component {
                forEach(notAvailEle, function (item) {
                     dlgMessage += '\n' + item;
                });
-               throw new Error(dlgMessage);
+               // throw new Error(dlgMessage);
+               // app.dialogs.showAlertDialog(dlgMessage);
           }
 
           /* Combine classes and classes that class's attribute type is Core Data Type */
@@ -233,7 +234,7 @@ class Component {
                     dlgMessage += '\n' + item;
                });
                // throw new Error(dlgMessage);
-               app.dialogs.showAlertDialog(dlgMessage);
+               // app.dialogs.showAlertDialog(dlgMessage);
           }
 
           /* Combine classes and classes that class's attribute type is Core Data Type */
